169d4529c05bb366b70d9ee47ef189e05b75697a
Rando Shtishi <rando.shtishi@dexi.io>
2022-12-01 10:26:37 +0100

    added model classes

--- backend/src/main/java/com/github/rshtishi/backend/model/Message.java
+++ backend/src/main/java/com/github/rshtishi/backend/model/Message.java
@@ -1,17 +1,24 @@
 package com.github.rshtishi.backend.model;
 
+import com.github.rshtishi.backend.enums.MessageStatus;
 import com.github.rshtishi.backend.enums.MessageType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Builder
 @Getter
 @Setter
 public class Message {
 
-    private MessageType type;
+    private String id;
     private String content;
     private String sender;
-    private String time;
+    private String recipient;
+    private Date created;
+    private MessageType type;
+    private MessageStatus status;
+
 }
