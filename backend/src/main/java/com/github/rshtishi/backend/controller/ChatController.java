f3bd01370bb506eb9caa44632d7a79aeb7f2dd47
Rando Shtishi <rando.shtishi@dexi.io>
2022-12-01 11:12:27 +0100

    modified rest controller

--- backend/src/main/java/com/github/rshtishi/backend/controller/ChatController.java
+++ backend/src/main/java/com/github/rshtishi/backend/controller/ChatController.java
@@ -1,26 +1,40 @@
 package com.github.rshtishi.backend.controller;
 
 import com.github.rshtishi.backend.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat.send")
-    @SendTo("/topic/public")
-    public Message sentMessage(@Payload final Message message) {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/message/public") // url -> /app/message/public
+    @SendTo("/jarks/public")
+    public Message receivePublicMessage(@Payload final Message message) {
         return message;
     }
 
+    @MessageMapping("/message")  // url -> /app/message/username
+    public Message receiveMessage(@Payload Message message) {
+        simpMessagingTemplate.convertAndSend(message.getRecipient(), message);
+        return message;
+    }
+
+    /*
     @MessageMapping("/chat.connect")
     @SendTo("/topic/public")
     public Message connect(@Payload final Message message, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", message.getSender());
         return message;
     }
+
+     */
 
 }
