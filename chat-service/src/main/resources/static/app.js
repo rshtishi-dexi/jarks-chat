df0f0133115a392a8733eb54314ccb94dba72626
Rando Shtishi <rando.shtishi@dexi.io>
2022-12-23 17:26:52 +0100

    partially implementation of join, it is retrieve users and update the status

--- chat-service/src/main/resources/static/app.js
+++ chat-service/src/main/resources/static/app.js
@@ -1,6 +1,3 @@
-var stompClient = null;
-
-
 function setConnected(connected) {
     localStorage.setItem("connected", JSON.stringify(connected));
 }
@@ -9,13 +6,43 @@ function getConnected() {
     return JSON.parse(localStorage.getItem("connected"));
 }
 
-function onConnected() {
+function refreshView() {
     if (getConnected()) {
-        $("#chat-page").show();
+        $("#chat-page").show()
         $("#login-page").hide();
     } else {
         $("#chat-page").hide();
         $("#login-page").show();
+    }
+}
+
+function onJoinedUser(user) {
+    /**
+     * TO DO
+     * add check for user input
+     */
+    debugger;
+    users = localStorage.getItem("users");
+    if (Array.isArray(users)) {
+        users = JSON.parse(users);
+        users.push(user);
+    } else {
+        users = [];
+        users.push(user);
+    }
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+function onLeftUser(user) {
+    /**
+     * TO DO
+     * add check for input
+     */
+    users = JSON.parse(localStorage.getItem("users"));
+    if (Array.isArray(users)) {
+        const index = users.indexOf(user);
+        users.splice(index, 1);
+        localStorage.setItem("users", users);
     }
 }
 
@@ -108,8 +135,10 @@ function showUsers(sender, type) {
 
 $(function () {
 
+    var socket, stompClient;
+
     //display correct view
-    onConnected();
+    refreshView();
 
     $("#login-form").on('submit', function (e) {
 
@@ -124,13 +153,34 @@ $(function () {
              */
 
             //Connect
-            var socket = new SockJS('/jarks-ws');
+            socket = new SockJS('/jarks-ws');
             stompClient = Stomp.over(socket);
+            console.log(socket);
+            console.log(stompClient);
             stompClient.connect({}, function (frame) {
 
                 //change view
                 setConnected(true);
-                onConnected();
+                refreshView();
+
+                //send join event
+                stompClient.send("/app/join", {}, JSON.stringify({
+                    'sender': username,
+                    'type': 'JOIN'
+                }));
+
+                //subscribe to events channel
+                stompClient.subscribe("/events", function (message) {
+                    var message = JSON.parse(message.body);
+                    if (message.type === "JOIN") {
+                        onJoinedUser(message.sender);
+                    } else if (message.type == "LEFT") {
+                        /**
+                         * TO DO
+                         * Add logic for LEFT EVENT
+                         */
+                    }
+                });
 
             });
 
@@ -139,5 +189,11 @@ $(function () {
 
     });
 
-    $()
+    $("#signOut").on("click", function () {
+        stompClient.disconnect();
+        setConnected(false);
+        refreshView();
+    })
+
+
 });
