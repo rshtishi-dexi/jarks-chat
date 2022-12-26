e31e1b34cd5d17a4945f323067f7f0de7289ceac
Rando Shtishi <rando.shtishi@dexi.io>
2022-12-26 20:38:27 +0100

    added test for findById repository method

--- cache-service/src/main/java/io/dexi/repository/UserRepository.java
+++ cache-service/src/main/java/io/dexi/repository/UserRepository.java
@@ -4,6 +4,10 @@ import io.dexi.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
+
 }
--- cache-service/src/test/java/io/dexi/repository/UserRepositoryIT.java
+++ cache-service/src/test/java/io/dexi/repository/UserRepositoryIT.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -18,7 +20,7 @@ class UserRepositoryIT {
     private UserRepository userRepository;
 
     @Test
-    void test() {
+    void when_userIsValid_then_saveWillSucceed() {
         //setup
         User user = new User("test", "");
         //execute
@@ -26,5 +28,17 @@ class UserRepositoryIT {
         //verify
         assertThat(savedUser).isNotNull();
         assertThat(savedUser.getUsername()).isEqualTo(user.getUsername());
+    }
+
+    @Test
+    void when_userWithUsernameIsPresent_findByIdWillReturnUser() {
+        //setup
+        User user = new User("test", "");
+        user = userRepository.save(user);
+        //execute
+        User userFound = userRepository.findById(user.getUsername()).get();
+        //verify
+        assertThat(userFound).isNotNull();
+        assertThat(userFound.getUsername()).isEqualTo(userFound.getUsername());
     }
 }
